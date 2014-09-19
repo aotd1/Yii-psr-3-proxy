0bb840da6ef3a141f56a07bab5b3c3db9eefa2b5
Alexey Ashurok <me@aotd.ru>
2014-09-19 12:08:13 +0700

    Initial commit

---
+++ YiiPsr3Proxy.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * This is a simple Logger proxy implementation.
+ */
+class YiiPsr3Proxy extends CApplicationComponent {
+
+    const EMERGENCY = 'emergency';
+    const ALERT = 'alert';
+    const CRITICAL = 'critical';
+    const ERROR = 'error';
+    const WARNING = 'warning';
+    const NOTICE = 'notice';
+    const INFO = 'info';
+    const DEBUG = 'debug';
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function emergency($message, array $context = array())
+    {
+        $this->log(self::EMERGENCY, $message, $context);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function alert($message, array $context = array())
+    {
+        $this->log(self::ALERT, $message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function critical($message, array $context = array())
+    {
+        $this->log(self::CRITICAL, $message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function error($message, array $context = array())
+    {
+        $this->log(self::ERROR, $message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function warning($message, array $context = array())
+    {
+        $this->log(self::WARNING, $message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function notice($message, array $context = array())
+    {
+        $this->log(self::NOTICE, $message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function info($message, array $context = array())
+    {
+        $this->log(self::INFO, $message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function debug($message, array $context = array())
+    {
+        $this->log(self::DEBUG, $message, $context);
+    }
+
+    public function log($level, $message, array $context = array())
+    {
+        Yii::log($message, self::logLevel($level), isset($context['category']) ? $context['category'] : 'application');
+    }
+
+    private static function logLevel($psrLevel)
+    {
+        switch ($psrLevel) {
+            case 'emergency': return CLogger::LEVEL_ERROR;
+            case 'alert': return CLogger::LEVEL_ERROR;
+            case 'critical': return CLogger::LEVEL_ERROR;
+            case 'error': return CLogger::LEVEL_ERROR;
+            case 'warning': return CLogger::LEVEL_WARNING;
+            case 'notice': return CLogger::LEVEL_INFO;
+            case 'info': return CLogger::LEVEL_INFO;
+            case 'debug': return CLogger::LEVEL_TRACE;
+        }
+        return CLogger::LEVEL_ERROR;
+    }
+}
